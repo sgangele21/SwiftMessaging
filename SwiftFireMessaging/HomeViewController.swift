ce42922ec13e071a534f1c49ad4128c1da818560
Sgangele21 <sahil.gangele@gmail.com>
2017-04-06 23:16:44 -0400

    God rid of using google user sign in, now it's just any email password combo. Created checks with alertviews, create a rootviewcontroller for the login screen.

---
+++ SwiftFireMessaging/HomeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  SwiftFireMessaging
+//
+//  Created by Sahil Gangele on 4/6/17.
+//  Copyright © 2017 Sahil Gangele. All rights reserved.
+//
+import UIKit
+
+public class HomeViewController: UINavigationController {
+ 
+}
