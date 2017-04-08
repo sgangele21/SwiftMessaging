20a4d29075a2e2decb8869d42c2f6200e0f0c6a6
Sgangele21 <sahil.gangele@gmail.com>
2017-04-08 15:04:12 -0400

    Finshed basic firebase integration, with users now showing up authenticated inisde of the app. Now able to display their username, and ability to sign them out. Created unimplemented view controller to create a new message with another user.

--- SwiftFireMessaging/HomeViewController.swift
+++ SwiftFireMessaging/HomeViewController.swift
@@ -6,7 +6,16 @@
 //  Copyright © 2017 Sahil Gangele. All rights reserved.
 //
 import UIKit
+import FirebaseAuth
 
 public class HomeViewController: UINavigationController {
  
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override public func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    
 }
---
+++ SwiftFireMessaging/NewMessageViewController.swift
@@ -0,0 +1,14 @@
+//
+//  NewMessageViewController.swift
+//  SwiftFireMessaging
+//
+//  Created by Sahil Gangele on 4/8/17.
+//  Copyright © 2017 Sahil Gangele. All rights reserved.
+//
+
+import UIKit
+
+public class NewMessageViewController: UITableViewController {
+    
+    // Implement
+}
